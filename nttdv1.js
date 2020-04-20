b52b05557c106636d897c6872ef851a278d49e3e
dmitrii <dmitrii.golikov91@gmail.com>
2020-04-20 17:10:21 +0300

    Working version

--- nttdv1.js
+++ nttdv1.js
@@ -14,6 +14,10 @@ document.addEventListener('DOMContentLoaded', function () {
     let userCircle = getCircle();
     let form = document.getElementById("counter");
     form.onchange = countBalls;
+    let newGameButton = document.getElementById("newGame");
+    newGameButton.onclick = newGame;
+    let engine;
+    let gameText = document.getElementById("game-text");
 
     function countBalls() {
         count = form.value;
@@ -44,6 +48,7 @@ document.addEventListener('DOMContentLoaded', function () {
             dx: dx,
             dy: dy,
             fillStile: healColor,
+            quarantineTime : 0,
             move: function () {
                 ctx.beginPath();
                 ctx.arc(this.x, this.y, ballRadius, 0, Math.PI * 2);
@@ -52,8 +57,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 ctx.closePath();
             },
             changeVector: function () {
-                this.dx = -this.dx;
-                this.dy = -this.dy;
+                if (Math.random() > 0.5) this.dx = -this.dx;
+                if (Math.random() > 0.5) this.dy = -this.dy;
             }
         }
     }
@@ -83,7 +88,7 @@ document.addEventListener('DOMContentLoaded', function () {
             circle.y > userCircle.y - 50 && circle.y < userCircle.y + ballRadius + 50) {
             circle.x += circle.dx;
             circle.fillStile = userCircle.fillStile;
-            if (Math.random() > 0.5) circle.y += circle.dy;
+            circle.y += circle.dy;
         }
     }
 
@@ -92,6 +97,8 @@ document.addEventListener('DOMContentLoaded', function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall(userCircle);
         for (let i = 0; i < circles.length; i++) {
+            let tempX= circles[i].x;
+            let tempY= circles[i].y;
             separate(circles[i]);
             if (circles[i].x + circles[i].dx > canvas.width - ballRadius ||
                 circles[i].x + circles[i].dx < ballRadius) {
@@ -103,12 +110,42 @@ document.addEventListener('DOMContentLoaded', function () {
                 circles[i].dy = -circles[i].dy;
                 if (Math.random() > 0.5) circles[i].dx = -circles[i].dx;
             }
+            if (tempX===circles[i].x&&tempY===circles[i].y) circles[i].quarantineTime++;
+            if (circles[i].quarantineTime>500) {
+                circles[i].quarantineTime=0;
+                circles[i].fillStile=healColor;
+            }
             circles[i].move();
+        }
+        if (circles.filter(circle => circle.fillStile===illColor).length===circles.length) {
+            clearInterval(engine);
+            gameText.setAttribute("style", "display: block");
+            setTimeout(gameOver, 3000);
         }
     }
 
-    fillcircles();
-    userCircle.fillStile = illColor;
-    setInterval(draw, 5);
+    function gameOver() {
+        canvas.setAttribute("style", "display: none");
+        newGameButton.setAttribute("style", "display: block");
+        gameText.setAttribute("style", "display: block");
+    }
+
+    function newGame() {
+    clearInterval(engine);
+    run();
+        canvas.setAttribute("style", "display: block");
+        newGameButton.setAttribute("style", "display: none");
+        gameText.setAttribute("style", "display: none");
+    }
+
+    function run() {
+        fillcircles();
+        userCircle.fillStile = illColor;
+        engine = setInterval(draw, 5);
+
+    }
+
+    run();
+
 });
 
