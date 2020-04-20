24735475a0309946824787c36f138293c679ad96
dmitrii <dmitrii.golikov91@gmail.com>
2020-04-20 15:48:09 +0300

    count added

--- nttdv1.js
+++ nttdv1.js
@@ -3,17 +3,27 @@ document.addEventListener('DOMContentLoaded', function () {
     let canvas = document.getElementById("nttd");
     let ctx = canvas.getContext("2d");
     let ballRadius = 5;
-    let x = canvas.width-ballRadius;
-    let y = canvas.height-ballRadius ;
-    let circles =[];
-    let count = 50;
+    let x = canvas.width - ballRadius;
+    let y = canvas.height - ballRadius;
+    let circles = [];
+    let count = 10;
     let dx = 1;
     let dy = -1;
+    let illColor = "#cd3333";
+    let healColor = "#9bcd9b";
     let userCircle = getCircle();
+    let form = document.getElementById("counter");
+    form.onchange = countBalls;
+
+    function countBalls() {
+        count = form.value;
+        circles = [];
+        fillcircles();
+    }
 
     function fillcircles() {
         for (let i = 0; i < count; i++) {
-            circles[i]=getCircle();
+            circles[i] = getCircle();
             console.log(circles[i]);
         }
 
@@ -21,19 +31,19 @@ document.addEventListener('DOMContentLoaded', function () {
 
     function mouseMoveHandler(e) {
         let rect = e.target.getBoundingClientRect();
-        userCircle.x = e.clientX-rect.left;
-        userCircle.y = e.clientY-rect.top;
+        userCircle.x = e.clientX - rect.left;
+        userCircle.y = e.clientY - rect.top;
     }
 
     function getCircle() {
         return {
             ballRadius: ballRadius,
             ctx: ctx,
-            x: Math.random()*x+ballRadius,
-            y: Math.random()*y+ballRadius,
+            x: Math.random() * x + ballRadius,
+            y: Math.random() * y + ballRadius,
             dx: dx,
             dy: dy,
-            fillStile: "#"+Math.floor(Math.random()*16777215).toString(16),
+            fillStile: healColor,
             move: function () {
                 ctx.beginPath();
                 ctx.arc(this.x, this.y, ballRadius, 0, Math.PI * 2);
@@ -42,8 +52,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 ctx.closePath();
             },
             changeVector: function () {
-             this.dx = -this.dx;
-             this.dy = -this.dy;
+                this.dx = -this.dx;
+                this.dy = -this.dy;
             }
         }
     }
@@ -57,18 +67,23 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function separate(circle) {
-        let separatedCircles = circles.filter(circle1 => circle1!==circle)
-        for (let i = 0; i <separatedCircles.length ; i++) {
-            if (circle.x > separatedCircles[i].x-50 && circle.x < separatedCircles[i].x+ballRadius+50 &&
-                circle.y > separatedCircles[i].y-50 && circle.y < separatedCircles[i].y+ballRadius+50) {
+        let separatedCircles = circles.filter(circle1 => circle1 !== circle)
+        for (let i = 0; i < separatedCircles.length; i++) {
+            if (circle.x > separatedCircles[i].x - 50 && circle.x < separatedCircles[i].x + ballRadius + 50 &&
+                circle.y > separatedCircles[i].y - 50 && circle.y < separatedCircles[i].y + ballRadius + 50) {
+                if (circle.fillStile === illColor || separatedCircles[i].fillStile === illColor) {
+                    circle.fillStile = illColor;
+                    separatedCircles[i].fillStile = illColor;
+                }
                 circle.x += circle.dx;
                 circle.y += circle.dy;
             }
         }
-        if (circle.x > userCircle.x-50 && circle.x < userCircle.x+ballRadius+50 &&
-            circle.y > userCircle.y-50 && circle.y < userCircle.y+ballRadius+50) {
+        if (circle.x > userCircle.x - 50 && circle.x < userCircle.x + ballRadius + 50 &&
+            circle.y > userCircle.y - 50 && circle.y < userCircle.y + ballRadius + 50) {
             circle.x += circle.dx;
-            if (Math.random()>0.5) circle.y += circle.dy;
+            circle.fillStile = userCircle.fillStile;
+            if (Math.random() > 0.5) circle.y += circle.dy;
         }
     }
 
@@ -76,23 +91,24 @@ document.addEventListener('DOMContentLoaded', function () {
     function draw() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall(userCircle);
-        for (let i = 0; i < circles.length ; i++) {
-           separate(circles[i]);
+        for (let i = 0; i < circles.length; i++) {
+            separate(circles[i]);
             if (circles[i].x + circles[i].dx > canvas.width - ballRadius ||
                 circles[i].x + circles[i].dx < ballRadius) {
                 circles[i].dx = -circles[i].dx;
-                if (Math.random()>0.5) circles[i].dy = -circles[i].dy;
+                if (Math.random() > 0.5) circles[i].dy = -circles[i].dy;
             }
             if (circles[i].y + circles[i].dy > canvas.height - ballRadius ||
-                circles[i].y + circles[i].dy < ballRadius ) {
+                circles[i].y + circles[i].dy < ballRadius) {
                 circles[i].dy = -circles[i].dy;
-                if (Math.random()>0.5) circles[i].dx = -circles[i].dx;
+                if (Math.random() > 0.5) circles[i].dx = -circles[i].dx;
             }
             circles[i].move();
         }
     }
 
     fillcircles();
+    userCircle.fillStile = illColor;
     setInterval(draw, 5);
 });
 
